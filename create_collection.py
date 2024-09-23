b85de5ff900a6cc9366f9bca7454029e2a5fda2d
Thiago Auler <thiagoauler@users.noreply.github.com>
2024-09-22 21:57:18 -0300

    First commit

--- create_collection.py
+++ create_collection.py
@@ -1,10 +1,1 @@
-import sys
-
-# Verifica se há argumentos passados
-if len(sys.argv) > 1:
-    # sys.argv[0] é o nome do script
-    # sys.argv[1] é o primeiro argumento passado
-    argumento = sys.argv[1]
-    print(f"Argumento recebido: {argumento}")
-else:
-    print("Nenhum argumento foi passado.")
+print('MiSTer Collection')
